edfd7b5492fe0b7a3dfdc66bddd6dcf86416531f
Gius-peppe De Marco <demarcog83@gmail.com>
2015-12-11 16:24:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,36 +1,27 @@
 // add this in the header of the template's html code
-
 <script>
-
-
-  function login_check() {
-
-    jQuery.post(ajaxurl, data, function(response) {
+		
+	  	var data = {
+    action: 'is_user_logged_in'
+		};
+	
+	  function login_check() {
+	  
+	jQuery.post(ajaxurl, data, function(response) {
     if(response == 'yes') {
-      // user is logged in, do your stuff here
-      console.log('logged');
-
-      jQuery('.hide_if_logged_line').hide('slow', function(){ $target.remove(); });
-      jQuery('.hide_if_logged_cell').hide('slow', function(){ $target.remove(); });
-
-
+        // user is logged in, do your stuff here
+	  console.log('logged');
+	  
+	  jQuery('.hide_if_logged_line').hide('slow', function(){ $target.remove(); });
+	  jQuery('.hide_if_logged_cell').hide('slow', function(){ $target.remove(); });
+	  
+	  
     } else {
         // user is not logged in, show login form here
-      console.log('not logged');
+	  console.log('not logged');
     }
 });
-
-      }
-
-       </script>
-
-  <script>
-
-    jQuery(document).ready(function() {
-        console.log( "ready!" );
-
-          login_check();
-
-    });
-
-  </script>
+		
+	  }
+	  
+   	</script>
